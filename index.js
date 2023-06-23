6c4b3e7df273eef7b6fce9144239ac71b343a443
Vaishali Vaidya <vaishalivaidya1996@gmail.com>
2023-06-23 16:12:45 +0530

    add

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ import productRouter from "./Routes/productRoutes.js";
 import userRouter from "./Routes/userRouter.js";
 import orderRouter from "./Routes/orderRoutes.js";
 import dummydataRoutes from "./Routes/dummyDataRoutes.js";
- import cors from "cors"
+import cors from "cors"
 
 // dotenv.config();
 const app = express();
-app.use(cors)
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use("/seed", dummydataRoutes)
